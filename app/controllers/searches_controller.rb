3f9b8b7c5949163d43948771b61b8afba32223ca
Ally Netzel <netzelally@gmail.com>
2018-12-21 02:27:09 +0000

    Automatically backed up by Learn

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -3,7 +3,7 @@ class SearchesController < ApplicationController
   end
 
   def foursquare
-    Faraday.get 'https://api.foursquare.com/v2/venues/search' do |req|
+    @resp = Faraday.get 'https://api.foursquare.com/v2/venues/search' do |req|
       req.params['client_id'] = UW12IFMM1RHV0GMD3TOIZIKWHPCIPIE5UYME5M21M5PLPJQC
       req.params['client_secret'] = S2PAXW20A5YR1XC4O11STDBDLQZGQW2SUW444D5FRNLMYJFS
       req.params['v'] = '20160201'
